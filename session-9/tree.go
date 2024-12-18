5c84b98051afccf58c7bca40e47eb67870fb8adf
gregor.vonfluee <gregor.vonfluee@adcubum.com>
2024-12-18 21:05:56 +0100

    tree

--- session-9/tree.go
+++ session-9/tree.go
@@ -1,1 +1,26 @@
-package session_9
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	height := 6 // Höhe des Baums
+	printTree(height)
+}
+
+func printTree(height int) {
+	// Zeichne die Baumkrone
+	for i := 0; i < height; i++ {
+		// Berechne die Anzahl der Sterne und Leerzeichen
+		spaces := strings.Repeat(" ", height-i-1)
+		stars := strings.Repeat("*", 2*i+1)
+
+		// Drucke die aktuelle Zeile
+		fmt.Println(spaces + stars)
+	}
+
+	// Zeichne den Stamm
+	fmt.Println(strings.Repeat(" ", height-1) + "|")
+}
